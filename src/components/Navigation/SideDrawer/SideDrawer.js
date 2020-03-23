7cf7ddc7df0163eadf9ab188fd68bea2ad8dab5e
marcus@NOTEBOOK <marcus11.04@web.de>
2020-03-23 16:23:36 +0100

    146. Creating a Responsive SideDrawer

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import classes from "./SideDrawer.css";
+const sideDrawer = (props) => {
+    // ... to conditionally attach different css classes to make sure we play some animation
+    return (
+        <div className={classes.SideDrawer}>
+            <Logo />
+            <nav>
+                <NavigationItems />
+            </nav>
+
+        </div>
+    );
+};
+export default sideDrawer;
