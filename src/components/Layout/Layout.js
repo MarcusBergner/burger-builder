d6c68e745e1571ba7aa8907faf00ae578e00637e
marcus@NOTEBOOK <marcus11.04@web.de>
2020-03-24 07:07:14 +0100

    149. Re-Using the Backdrop Component for adding SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import Auxiliary from "../../hoc/Auxiliary";
 import classes from "./Layout.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
-const layout = (props) => (
-    <Auxiliary>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Auxiliary>
-);
+class Layout extends Component {
 
-export default layout;
+    state = {
+        showSideDrawer: true
+    }
+    sideDrawerCloseHandler = () => {
+        this.setState({ showSideDrawer: false });
+    }
+
+    render() {
+        return (
+
+            <Auxiliary>
+                <Toolbar />
+                <SideDrawer open={this.state.showSideDrawer}
+                    closed={this.sideDrawerCloseHandler} />
+                <main className={classes.Content}>
+                    {/* only for class based components, reference with "this." */}
+                    {this.props.children}
+                </main>
+            </Auxiliary>
+        )
+    }
+};
+
+export default Layout;
