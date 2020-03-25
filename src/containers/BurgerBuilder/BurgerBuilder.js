3cc0aee9ffe37e448b0e65396a07f9ec618b6645
marcus@NOTEBOOK <marcus11.04@web.de>
2020-03-25 08:31:22 +0100

    181. Sending Post request to our Firebase Backend

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 const INGREDIENT_PRICES = {
     salad: 0.5,
     cheese: 0.4,
@@ -81,7 +82,32 @@ class BurgerBuilder extends Component {
         this.setState({ purchasing: false });
     }
     purchaseContinueHandler = () => {
-        alert("You continue!");
+        // alert("You continue!");
+        // not a setup in real-world() --> prices should definitely calculate on the server-site, for no manipualte !
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: "Marcus",
+                adress: {
+                    street: "Am Hamburger Bahnhof 4",
+                    zipCode: "10557",
+                    country: "Germany"
+                },
+                email: "test@test.com"
+            },
+            deliveryMethod: "fastest"
+        }
+        // send the data to my backend, ".json" -> only a firebase spezial thing for correct work!
+        axios.post("/orders.json", order)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+
+
     }
     render() {
         const disableInfo = {
