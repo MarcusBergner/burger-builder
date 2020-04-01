a69ddda110a3b1452a2d03b9b9b948bc0e237f2d
marcus@NOTEBOOK <marcus11.04@web.de>
2020-04-01 10:56:55 +0200

    224. Navigating Back & To Next Page from the OrderSummary after clicking continue

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -9,10 +9,30 @@ class Checkout extends Component {
       bacon: 1
     }
   };
+  /**
+   * goes back to the last page.
+   * @returns (go to previews page)
+   */
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack();
+  };
+  /**
+   * want to continue,
+   * use replace instead, to replace the current route
+   * -> to loads the contactData component.
+   * @returns (contactData component)
+   */
+  checkoutContinuedHandler = () => {
+    this.props.history.replace("/checkout/contact-data");
+  };
   render() {
     return (
       <div>
-        <CheckoutSummary ingredients={this.state.ingredients} />
+        <CheckoutSummary
+          ingredients={this.state.ingredients}
+          checkoutCancelled={this.checkoutCancelledHandler}
+          checkoutContinued={this.checkoutContinuedHandler}
+        />
       </div>
     );
   }
