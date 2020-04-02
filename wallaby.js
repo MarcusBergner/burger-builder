737050bc298b00edf966142a934598bb18459394
marcus@NOTEBOOK <marcus11.04@web.de>
2020-04-02 11:12:17 +0200

    229. Implementing Navigation Links

--- wallaby.js
+++ wallaby.js
@@ -1,18 +1,38 @@
-module.exports = function(wallaby) {
+// whenever wallaby loads the config file (and later uses babel),
+// babel will know that we are in the `test` env
+
+module.exports = wallaby => {
+  require(path.join(wallaby.localProjectDir, "test", "test_helper"));
+
+  const babel = require("babel");
   return {
     files: [
-      {
-        pattern: "node_modules/react/dist/react-with-addons.js",
-        instrument: false
-      },
-      "assets/**",
-      "src/**"
+      "src/*.js",
+      "test/test_helper.js" // <-- the file will now be processed by wallaby like the rest of files
     ],
 
-    tests: ["test/**/*Spec.jsx"],
+    tests: ["test/*spec.js"],
 
     compilers: {
-      "**/*.js*": wallaby.compilers.babel()
+      "**/*.js": wallaby.compilers.babel({
+        babel: babel,
+        stage: 0
+      })
+    },
+
+    workers: {
+      initial: 6,
+      regular: 2
+    },
+
+    env: {
+      type: "node"
+    },
+
+    testFramework: "mocha",
+
+    bootstrap: function bootstrap() {
+      require("./test/test_helper"); // <-- it's in the `files` list, so you may just do that
     }
   };
 };
