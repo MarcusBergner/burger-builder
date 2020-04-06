a354a5a24afdb8afe637351869d04c11e06b9cfd
marcus@NOTEBOOK <marcus11.04@web.de>
2020-04-06 10:37:21 +0200

    Fixing a Common Validation Gotch in ContactData.js

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -136,15 +136,15 @@ class ContactData extends Component {
    * @returns true or false
    */
   checkValidity(value, rules) {
-    let isValid = false;
+    let isValid = true;
     if (rules.required) {
-      isValid = value.trim() !== "";
+      isValid = value.trim() !== "" && isValid;
     }
     if (rules.minLength) {
-      isValid = value.length >= rules.minLength;
+      isValid = value.length >= rules.minLength && isValid;
     }
     if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength;
+      isValid = value.length <= rules.maxLength && isValid;
     }
     return isValid;
   }
