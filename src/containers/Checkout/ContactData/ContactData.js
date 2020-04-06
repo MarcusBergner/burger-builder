e8af485a540b7d417fe596768b1653406764a7d6
marcus@NOTEBOOK <marcus11.04@web.de>
2020-04-06 07:47:05 +0200

    238. Dynamically Create Inputs based on JS Confog

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -13,10 +13,9 @@ import Input from "../../../components/UI/Input/Input";
  *  important that you use them inside elementConfig the default names!
  */
 class ContactData extends Component {
+  // TODO create a helper method for initialize it
   state = {
     orderForm: {
-      // TODO create a helper method for initialize it
-
       name: {
         elementType: "input",
         elementConfig: {
@@ -99,28 +98,28 @@ class ContactData extends Component {
       });
     // console.log(this.props.ingredients);
   };
+  /**
+   * @for(...) return the property-names of that orderForm Object.
+   */
   render() {
+    const formElementsArray = [];
+    for (let key in this.state.orderForm) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key]
+      });
+    }
     let form = (
       <form>
-        <Input elementType="..." elementConfig="..." value=".." />
-        <Input
-          inputtype="input"
-          type="email"
-          name="email"
-          placeholder="Your email"
-        />
-        <Input
-          inputtype="input"
-          type="text"
-          name="street"
-          placeholder="Your street"
-        />
-        <Input
-          inputtype="input"
-          type="text"
-          name="postalCode"
-          placeholder="Your postalCode"
-        />
+        {formElementsArray.map(formElement => (
+          <Input
+            key={formElement.id}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+          />
+        ))}
+
         <Button btnType="Success" clicked={this.orderHandler}>
           ORDER
         </Button>
