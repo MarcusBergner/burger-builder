c7256b260bbbb4b6db42cbdfa945cff9a94fbdb5
marcus@NOTEBOOK <marcus11.04@web.de>
2020-04-09 05:44:02 +0200

    274. Installing Redux and React-Redux

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -5,27 +5,33 @@ import BuildControl from "./BuildControl/BuildControl";
 // will be a funcional component
 
 const controls = [
-    { label: "Salad", type: "salad" },
-    { label: "Bacon", type: "bacon" },
-    { label: "Cheese", type: "cheese" },
-    { label: "Meat", type: "meat" },
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" },
 ];
 
-
 const buildControls = (props) => (
-    <div className={classes.BuildControls}>
-        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
-        {controls.map(crtl => (
-            <BuildControl
-                key={crtl.label}
-                label={crtl.label}
-                added={() => props.ingredientAdded(crtl.type)}
-                removed={() => props.ingredientRemoved(crtl.type)}
-                disabled={props.disabled[crtl.type]} />
-        ))}
-        <button className={classes.OrderButton}
-            disabled={!props.purchasable}
-            onClick={props.ordered}>ORDER NOW</button>
-    </div>
+  <div className={classes.BuildControls}>
+    <p>
+      Current Price: <strong>{props.price.toFixed(2)} €</strong>
+    </p>
+    {controls.map((crtl) => (
+      <BuildControl
+        key={crtl.label}
+        label={crtl.label}
+        added={() => props.ingredientAdded(crtl.type)}
+        removed={() => props.ingredientRemoved(crtl.type)}
+        disabled={props.disabled[crtl.type]}
+      />
+    ))}
+    <button
+      className={classes.OrderButton}
+      disabled={!props.purchasable}
+      onClick={props.ordered}
+    >
+      ORDER NOW
+    </button>
+  </div>
 );
 export default buildControls;
