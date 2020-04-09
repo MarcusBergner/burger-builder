e6606d13284222262ff262a3b4e3d14553aea98f
marcus@NOTEBOOK <marcus11.04@web.de>
2020-04-09 05:57:41 +0200

    275. Basic Redux set up in index.js

--- src/index.js
+++ src/index.js
@@ -4,14 +4,30 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./store/reducer";
 
 /**
+ * connecting our store which is created by redux with our
+ * react-app!
+ * Also set the store property on the provider component and
+ * pass our created store.
+ */
+const store = createStore(reducer);
+/**
  * For activate Routing in app, use BrowserRouter and wrapped!
+ * Spezial Import if use Provider.react-redux & BrowserRouter.react-router,
+ * making sure that the connect functionality and the routing functionalities work
+ * together fine because both implicitly set up some props on the wrapping component,
+ * so we have to make sure that everything works!
  */
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 ReactDOM.render(app, document.getElementById("root"));
 registerServiceWorker();
