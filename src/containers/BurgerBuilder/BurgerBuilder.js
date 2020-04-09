d62c1c86f2499f0393ae4bf9c0c9a295041dd54d
marcus@NOTEBOOK <marcus11.04@web.de>
2020-04-09 07:49:03 +0200

    279. Redux & UI State, fixing button is showing in BurgerBuilder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -19,7 +19,6 @@ class BurgerBuilder extends Component {
   // }
 
   state = {
-    purchasable: false,
     purchasing: false,
     loading: false,
     error: false,
@@ -43,7 +42,7 @@ class BurgerBuilder extends Component {
       .reduce((sum, el) => {
         return sum + el;
       }, 0);
-    this.setState({ purchasable: sum > 0 });
+    return sum > 0;
   }
 
   purchaseHandler = () => {
@@ -104,7 +103,7 @@ class BurgerBuilder extends Component {
             ingredientAdded={this.props.onIngredientAdded}
             ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disableInfo}
-            purchasable={this.state.purchasable}
+            purchasable={this.updatePuchaseState(this.props.ings)}
             ordered={this.purchaseHandler}
             price={this.props.price}
           />
