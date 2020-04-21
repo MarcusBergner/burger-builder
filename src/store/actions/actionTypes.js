a560d6f7e95206b17ddaddcd77ceafb52bdc83c7
marcus@NOTEBOOK <marcus11.04@web.de>
2020-04-21 06:47:25 +0200

    314 Resetting the Price after Purchases

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -1,7 +1,7 @@
 export const ADD_INGEDRIENT = "ADD_INGEDRIENT";
 export const REMOVE_INGEDRIENT = "REMOVE_INGEDRIENT";
 /**
- * action Type
+ * the action Type is dipatched whenever this page is loaded.
  */
 export const SET_INGREDIENTS = "SET_INGREDIENTS";
 export const FETCH_INGREDIENTS_FAILED = "FETCH_INGREDIENTS_FAILED";
@@ -10,6 +10,6 @@ export const PURCHASE_BURGER_START = "PURCHASE_BURGER_START";
 export const PURCHASE_BURGER_SUCCESS = "PURCHASE_BURGER_SUCCESS";
 export const PURCHASE_BURGER_FAIL = "PURCHASE_BURGER_FAIL";
 /**
- * will be dipatched whenever we load the checkout page
+ * the action Type will be dipatched whenever we load the checkout page
  */
 export const PURCHASE_INIT = "PURCHASE_INIT";
