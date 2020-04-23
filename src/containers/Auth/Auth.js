aa10ab72e296ad4ce9556e8ac12f5b619d4ffd3a
marcus@NOTEBOOK <marcus11.04@web.de>
2020-04-23 08:35:37 +0200

    234 Adding Auth Form for Validation User-Authentication

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Input from "../../components/UI/Input/Input";
 import Button from "../../components/UI/Button/Button";
+import classes from "./Auth.css";
 class Auth extends Component {
   state = {
     controls: {
@@ -34,9 +35,59 @@ class Auth extends Component {
       },
     },
   };
+
   /**
-   * @formElementsArray  Converted th State-Object to an Array,
-   * Then i can loop through all our controls.
+   *
+   * @param {*UserInputs} value
+   * @param {*Validation} rules
+   */
+  checkValidity(value, rules) {
+    let isValid = true;
+    if (rules.required) {
+      isValid = value.trim() !== "" && isValid;
+    }
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid;
+    }
+
+    if (rules.isNumeric) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid;
+    }
+    return isValid;
+  }
+
+  /**
+   * Function that update the induvidual control,
+   * which was changed.
+   */
+  inputChangedHandler = (event, controlName) => {
+    const updatedControls = {
+      ...this.state.controls,
+      [controlName]: {
+        ...this.state.controls[controlName],
+        value: event.target.value,
+        valid: this.checkValidity(
+          event.target.value,
+          this.state.controls[controlName].validation
+        ),
+        touched: true,
+      },
+    };
+    this.setState({ controls: updatedControls });
+  };
+
+  /**
+   * @formElementsArray  Converted the State-Object to an Array,
+   * Then i can loop through all our controls and create a Auth-form-Object.
+   * @form is a Array  for a dynamicaly set of inputs.
    */
   render() {
     const formElementsArray = [];
@@ -63,7 +114,7 @@ class Auth extends Component {
       />
     ));
     return (
-      <div>
+      <div className={classes.AuthData}>
         <form>
           {form}
           <Button btnType="Success">SUBMIT</Button>
