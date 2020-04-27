2603802123c653bdd285fb83e3d2df2a5797b12f
marcus@NOTEBOOK <marcus11.04@web.de>
2020-04-27 07:09:53 +0200

    328 Storing the Token

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -8,10 +8,11 @@ export const authStart = () => {
   };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (idToken, userId) => {
   return {
     type: actionTypes.AUTH_SUCCESS,
-    authData: authData,
+    token: idToken,
+    userId: userId,
   };
 };
 
@@ -43,7 +44,7 @@ export const auth = (email, password, isSignup) => {
       // success-case, inside then()
       .then((response) => {
         console.log(response);
-        dispatch(authSuccess(response));
+        dispatch(authSuccess(response.data.idToken, response.data.localId));
       })
       .catch((error) => {
         console.log(error);
