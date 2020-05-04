a1a261d1247296fc9134f63f4d7016e7443ebf3a
marcus@NOTEBOOK <marcus11.04@web.de>
2020-05-04 06:15:04 +0200

    347 Using enviroment Variable to use React-dev-tools only if are in develop mode

--- src/index.js
+++ src/index.js
@@ -14,7 +14,10 @@ import authReducer from "./store/reducers/auth";
 /**
  * @compose allows us to compose our own set of enchancers and middleware
  */
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose;
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
   order: orderReducer,
