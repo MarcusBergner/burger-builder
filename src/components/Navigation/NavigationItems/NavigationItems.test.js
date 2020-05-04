72509e63a432ed7af142865d4a1581beae5ddcd8
marcus@NOTEBOOK <marcus11.04@web.de>
2020-05-04 08:27:35 +0200

    357 testing Components Continued

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -5,11 +5,21 @@ import NavigationItems from "./NavigationItems";
 import NavigationItem from "./NavigationItem/NavigationItem";
 // conect enzyme
 configure({ adapter: new Adapter() });
-
+let wrapper;
+beforeEach(() => {
+  wrapper = shallow(<NavigationItems />);
+});
 describe("<NavigationItems />", () => {
   it("should render two <NavigationItems /> elements if not autnenticated", () => {
-    const wrapper = shallow(<NavigationItems />);
     // whrite expection
     expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+});
+describe("<NavigationItems />", () => {
+  it("should render three <NavigationItems /> elements if is autnenticated", () => {
+    // whrite expection
+    // wrapper = shallow(<NavigationItems isAuthenticated />);
+    wrapper.setProps({ isAuthenticated: true });
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
 });
