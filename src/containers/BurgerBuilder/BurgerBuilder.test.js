819d0eaa6b58a73a84384e8021225b873d2d58b5
marcus@NOTEBOOK <marcus11.04@web.de>
2020-05-18 06:43:16 +0200

    set correct package reference for webpack

--- src/containers/BurgerBuilder/BurgerBuilder.test.js
+++ src/containers/BurgerBuilder/BurgerBuilder.test.js
@@ -3,6 +3,7 @@ import { configure, shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import { BurgerBuilder } from "./BurgerBuilder";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+
 // conect enzyme
 configure({ adapter: new Adapter() });
 
