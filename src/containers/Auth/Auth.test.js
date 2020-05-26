48dc1cf7a3952ee8736a97087e462dd43e7f6f19
marcus@NOTEBOOK <marcus11.04@web.de>
2020-05-26 04:58:18 +0200

    set up color comment-plugin

--- src/containers/Auth/Auth.test.js
+++ src/containers/Auth/Auth.test.js
@@ -5,15 +5,15 @@ import Auth from "./Auth";
 
 // most cases react components are just functions, they only depend on the props they recive!!!
 // test example for a functional component
-
+// ? What are the right Test-setup ??
 // conect enzyme#
 configure({ adapter: new Adapter() });
 let wrapper;
 beforeEach(() => {
   wrapper = shallow(<Auth />);
-});
+}); /*?*/
 describe("<Auth />", () => {
   it("should.....", () => {
-    expect(wrapper.setProps({}));
+    expect(wrapper);
   });
-});
+}); /*?*/
