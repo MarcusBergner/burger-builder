0205c6935bf2be771e6a6d3b407262c3c7ce08e1
marcus@NOTEBOOK <marcus11.04@web.de>
2020-06-16 06:35:52 +0200

    454. Set up th App.js for use hooks & Routing with React.lazy() ans Suspens

--- src/App.js
+++ src/App.js
@@ -1,56 +1,60 @@
-import React, { Component } from "react";
+import React, { useEffect, Suspense } from "react";
 import { Route, Switch, withRouter, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
-import asyncComponent from "./hoc/asyncComponent/asyncComponent";
 
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Logout from "./containers/Auth/Logout/Logout";
 import * as actions from "./store/actions/index";
 
-const asyncCheckout = asyncComponent(() => {
+// ! note: React.lazy() does like the same as the asyncComponent, now i replace it for use React.hooks
+const Checkout = React.lazy(() => {
   return import("./containers/Checkout/Checkout");
 });
-const asyncOrders = asyncComponent(() => {
+const Orders = React.lazy(() => {
   return import("./containers/Orders/Orders");
 });
-const asyncAuth = asyncComponent(() => {
+const Auth = React.lazy(() => {
   return import("./containers/Auth/Auth");
 });
-class App extends Component {
-  componentDidMount() {
-    this.props.onTryAutoSignup();
-  }
 
-  render() {
-    let routes = (
+// ! important to know: that useEffect() -> runs when ever this component got re-rendered, after and for every comopnent-render-cycle!!
+// ! need the second args [] in this case -> it runs ONLY ONCE(after the first render) never after,
+// ! because if use not its return an infinit - loop !!
+const app = (props) => {
+  useEffect(() => {
+    props.onTryAutoSignup();
+  }, []);
+
+  let routes = (
+    <Switch>
+      <Route path="/auth" render={() => <Auth />} />
+      <Route path="/" exact component={BurgerBuilder} />
+      <Redirect to="/" />
+    </Switch>
+  );
+
+  if (props.isAuthenticated) {
+    routes = (
       <Switch>
-        <Route path="/auth" component={asyncAuth} />
+        <Route path="/checkout" render={() => <Checkout />} />
+        <Route path="/orders" render={() => <Orders />} />
+        <Route path="/logout" component={Logout} />
+        <Route path="/auth" render={() => <Auth />} />
         <Route path="/" exact component={BurgerBuilder} />
         <Redirect to="/" />
       </Switch>
     );
-
-    if (this.props.isAuthenticated) {
-      routes = (
-        <Switch>
-          <Route path="/checkout" component={asyncCheckout} />
-          <Route path="/orders" component={asyncOrders} />
-          <Route path="/logout" component={Logout} />
-          <Route path="/auth" component={asyncAuth} />
-          <Route path="/" exact component={BurgerBuilder} />
-          <Redirect to="/" />
-        </Switch>
-      );
-    }
-
-    return (
-      <div>
-        <Layout>{routes}</Layout>
-      </div>
-    );
   }
-}
+
+  return (
+    <div>
+      <Layout>
+        <Suspense fallback={<p>Loading...</p>}>{routes}</Suspense>
+      </Layout>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
@@ -70,4 +74,4 @@ const mapDispatchToProps = (dispatch) => {
  * @withRouter will enforce your props being passed down to your component still,
  *  therefore react router is back on page know what's getting loaded!
  */
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(app));
